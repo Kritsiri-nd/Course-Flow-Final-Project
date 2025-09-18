e89d2a671d4fdbcfb880565c9cac0744089efe74
First <first.ruankaeo@gmail.com>
2025-09-18 16:34:08 +0700

    feat: add new icons and components for enhanced UI, update homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 <<<<<<< HEAD
 =======
 import Navbar from "../components/ui/navbar";
-// import Instructure from "../components/ui/instructure";
-// import SubFooter from "../components/ui/subFooter";
+import Instructure from "../components/ui/instructure";
+import SubFooter from "../components/ui/subFooter";
 // import Footer from "../components/ui/footer";
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
-      {/* <Instructure />
+      <Instructure />
       <SubFooter />
+      {/* 
+      
       <Footer /> */}
     </>
   );
