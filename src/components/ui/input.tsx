76f12125049d45e1e8b3304e1548745b7d91f4bd
FSD9-Wave <loomaflex44@gmail.com>
2025-09-18 16:26:32 +0700

    feat: create registration page UI

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex w-[453px] h-12 rounded-lg border border-[#D6D9E4] bg-white pt-3 pr-4 pb-3 pl-3 font-inter font-normal text-base leading-[150%] tracking-[0%] text-[#9AA1B9] ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-[#9AA1B9] focus-visible:outline-none focus-visible:border-[#F47E20] disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Input.displayName = "Input"
+
+export { Input }
