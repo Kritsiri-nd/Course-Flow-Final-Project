d8465d06ad2e611049c50cdc8d34a2508160436a
First <first.ruankaeo@gmail.com>
2025-09-18 22:20:58 +0700

    refactor: navbar

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -6,29 +6,25 @@ import CourseFlowIcon from "../../assets/courseFlowIcon"
 export default function Navbar() {
   const router = useRouter()
 
-  const LoginButton = () => (
-    <button
-      className="w-[112px] h-[60px] rounded-[12px] bg-[#2F5FAC] text-white font-semibold text-base hover:bg-[#244a8f] transition-colors"
-      onClick={() => router.push("/auth/login")}
-    >
-      Log in
-    </button>
-  )
-
   return (
     <header className="h-[88px] bg-white shadow-md flex items-center justify-center">
       <nav className="w-[80vw] flex justify-between items-center">
         <div className="cursor-pointer" onClick={() => router.push("/")}>
           <CourseFlowIcon className="w-[140px] sm:w-[160px] md:w-[180px] hover:scale-105 transition-transform" />
         </div>
-        <div className="flex items-center space-x-8">
+        <div className="flex items-center justify-between gap-15">
           <div
             className="text-base font-bold text-[#191C77] cursor-pointer hover:scale-105 transition-transform"
             onClick={() => router.push("/public/courses")}
           >
             Our Courses
           </div>
-          <LoginButton />
+          <div
+            className="w-[112px] h-[60px] rounded-[12px] bg-[#2F5FAC] text-white font-semibold text-base cursor-pointer hover:scale-105 transition-transform flex items-center justify-center"
+            onClick={() => router.push("/auth/login")}
+          >
+            Log in
+          </div>
         </div>
       </nav>
     </header>
